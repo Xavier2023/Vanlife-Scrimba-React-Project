14fc23bb3e1d9c04c199327c41f3ae1f2a2d6017
Orjinta Anthony <orjintaxavier@gmail.com>
2024-05-29 22:32:29 +0100

    vans and vans details

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,12 @@ import { useState } from 'react'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
+import Vans from './pages/Vans'
+import VanDetail from './pages/VanDetail'
+
+import '../server'
 import './App.css'
+
 
 
 function App() {
@@ -13,11 +18,14 @@ function App() {
         <Link className='site-logo' to="/">#VANLIFE</Link>
         <nav>
           <Link to="/about">About</Link>
+          <Link to="/vans">Vans</Link>
         </nav>
       </header>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
+        <Route path='/vans' element={<Vans />} />
+        <Route path='/vans/:id' element={<VanDetail/>} />
       </Routes>
     </BrowserRouter>
   )
---
+++ src/pages/Vans.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+
+const Vans = () => {
+    const [vans, setVans] = useState([])
+
+    useEffect(() => {
+        const getVans = async () => {
+            const res = await fetch("/api/vans")
+            const data = await res.json()
+            setVans(data.vans)
+        }
+        getVans()
+    }, [])
+
+    const vanElements = vans.map((van) => (
+        <Link to={`/vans/${van.id}`}>
+            <div key={van.id} className="van-tile">
+            <img src={van.imageUrl} />
+            <div className="van-info">
+                <h3>{van.name}</h3>
+                <p>${van.price}<span>/day</span></p>
+            </div>
+            <i className={`van-type ${van.type} selected`}>{van.type}</i>
+        </div>
+        </Link>
+    ))
+  return (
+    <div className="van-list-container">
+        <h1>Explore our van options</h1>
+        <div className="van-list">
+            {vanElements}
+        </div>
+    </div>
+  )
+}
+
+export default Vans
